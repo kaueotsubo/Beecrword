5b46984d5f677da3b42db41eb9f4cf2863a2e1e4
Kauê otsubo <128756900+kaueotsubo@users.noreply.github.com>
2025-11-07 12:29:29 -0500

    Enhance blusas sorting and input processing
    Refactor sorting function and improve input handling.

--- Problema_1258.c
+++ Problema_1258.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 typedef struct{
     char nome[250];
@@ -7,14 +8,38 @@ typedef struct{
     char tamanho;
 }blusas;
 
-void ordenarBlusas(blusas lista[], int total){
+int comparar(const void *a, const void *b){
+    blusas *blusaA = (blusas *)a;
+    blusas *blusaB = (blusas *)b;
 
+    int cmpcor = strcmp(blusaA->cor, blusaB->cor);
+
+    if(cmpcor != 0){
+        return cmpcor;
+    }
+
+    if (blusaA->tamanho < blusaB->tamanho){
+        return 1;
+    }
+    if (blusaA->tamanho > blusaB->tamanho){
+        return -1;
+    }
+
+    return strcmp(blusaA->nome, blusaB->nome);    
+}
+
+void ordenarBlusas(blusas lista[], int total){
+    qsort(lista, total, sizeof(blusas), comparar);
 }
 
 int main() {
     int i, n, j;
-    scanf("%d", &n);
-    while(n != 0){
+    int caso = 1;
+    while(scanf("%d", &n) != EOF && n != 0){
+        if (!caso) {
+            printf("\n"); 
+        }
+        caso = 0;
         blusas turma[n];
         for(i=0;i<n;i++){
             scanf(" %[^\n]", turma[i].nome); 
@@ -22,9 +47,10 @@ int main() {
         }
         ordenarBlusas(turma, n);
 
-        
-        printf("\n");
-        scanf("%d", &n);
+        for(i=0; i<n; i++){
+            printf("%s %c %s\n", turma[i].cor, turma[i].tamanho, turma[i].nome);
+        }
+
     }
     return 0;
 }
